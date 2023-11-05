00c0a3a01334b797f653cc28e9cd2a2cf1ae71d9
nohhyejin <shgpwls374@gmail.com>
2023-11-05 17:40:04 +0900

    Update hello.py

--- hello.py
+++ hello.py
@@ -3,11 +3,13 @@ import time
 print("Hello!")
 print("It is", time.strftime('%Y-%m-%d %H:%M:%S'))
 print()
+print("Type 'terminate' or 'exit' if you want to exit :")
+print()
 
 while True:
-    print("Type your word:")
+    print("Type your word :")
     question = input()
-    if question == "terminate": break
+    if question in ("terminate", "exit"): break
     print()
     print("You typed:")
     print(question)
