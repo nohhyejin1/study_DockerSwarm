345e4ea33c5c0b78eca1fef01c7daa17e1c6caca
nohhyejin <shgpwls374@gmail.com>
2023-11-05 17:28:27 +0900

    Update hello.py

--- hello.py
+++ hello.py
@@ -3,8 +3,12 @@ import time
 print("Hello!")
 print("It is", time.strftime('%Y-%m-%d %H:%M:%S'))
 print()
-print("Type your word:")
-question = input()
-print()
-print("You typed:")
-print(question)
+
+while True:
+    print("Type your word:")
+    question = input()
+    if question == "terminate": break
+    print()
+    print("You typed:")
+    print(question)
+    time.sleep(1)
